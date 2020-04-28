0aef88b2a4f50ec2ca1bf09b9c116232696f0365
hgw <hasegawa@isp.co.jp>
2020-04-28 17:28:34 +0900

    add: init_thread and util

---
+++ include/ros2_timer_callback_latency_measurement/util.hpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+class Hist;
+class TimeSeries;
+
+
+class HistReport {
+ public:
+  HistReport(int bins_num, uint64_t bin_width_ns=1000, int max_topn=10);
+  void add(uint64_t ns);
+  void saveHist(std::string filename);
+  void saveTopN(std::string filename);
+
+private:
+  inline int getNextIdx(const int &i);
+  inline int getPrevIdx(const int &i);
+  std::vector<unsigned int> bins_;
+  int bins_num_;
+  uint64_t bin_width_ns_;
+
+  int idx_;
+  int max_topn_;
+  int head_idx_;
+  std::vector<uint64_t> top_ns_;
+  std::vector<int> top_idx_;
+};
+
+class TimeSeriesReport {
+ public:
+   TimeSeriesReport(int max_data_num);
+   void add(uint64_t ns);
+   void save(std::string filename);
+
+ private:
+   int max_data_num_;
+   int idx_;
+   std::vector<uint64_t> data_;
+};
---
+++ src/init_thread.cpp
@@ -0,0 +1,136 @@
+#include "rclcpp/executor.hpp"
+#include "rclcpp/executors.hpp"
+#include "rclcpp/node.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "ros2_timer_callback_latency_measurement/util.hpp"
+#include "rttest/rttest.h"
+#include "rttest/utils.h"
+#include <bits/stdint-uintn.h>
+#include <chrono>
+#include <getopt.h> // for getopt_long
+#include <limits.h>
+#include <rclcpp/strategies/allocator_memory_strategy.hpp>
+#include <rclcpp/strategies/message_pool_memory_strategy.hpp>
+#include <sched.h>
+#include <tlsf_cpp/tlsf.hpp>
+#include <type_traits>
+#include <unistd.h>
+#include <unistd.h> // for getopt
+
+#include <malloc.h>
+#include <sys/mman.h>
+
+#define TRUE 1
+#define FALSE 0
+
+using namespace std::chrono_literals;
+using namespace std;
+
+using rclcpp::memory_strategies::allocator_memory_strategy:: AllocatorMemoryStrategy;
+
+struct Params {
+  rttest_params rt;
+  bool realtime_child;
+  string hist_filename;
+  string topn_filename;
+  string timeseries_filename;
+};
+
+Params get_params(int argc, char *argv[]);
+
+template <typename T = void> using TLSFAllocator = tlsf_heap_allocator<T>;
+int main(int argc, char *argv[]) {
+  Params params = get_params(argc, argv);
+
+  if (params.realtime_child && rttest_set_thread_default_priority()) {
+    perror("Couldn't set scheduling priority and policy");
+    // return -1;
+  }
+  rclcpp::init(argc, argv);
+  if (!params.realtime_child && rttest_set_thread_default_priority()) {
+    perror("Couldn't set scheduling priority and policy");
+    // return -1;
+  }
+
+  HistReport hist(100);
+  TimeSeriesReport timeSeries(params.rt.iterations);
+
+  struct timespec expected, wakeup, wakeup_latency;
+  vector<uint64_t> wakeup_latencies(params.rt.iterations);
+
+  if (rttest_lock_and_prefault_dynamic() != 0) {
+    fprintf(stderr, "Couldn't lock all cached virtual memory. errno = %d \n",
+            errno);
+    fprintf(stderr, "Pagefaults from reading pages not yet mapped into RAM "
+                    "will be recorded.\n");
+  }
+
+  uint64_t latency;
+  clock_gettime(CLOCK_MONOTONIC, &expected);
+
+  for (unsigned long i = 0; i < params.rt.iterations; i++) {
+    add_timespecs(&expected, &params.rt.update_period, &expected);
+
+    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &expected, NULL);
+    clock_gettime(CLOCK_MONOTONIC, &wakeup);
+
+    subtract_timespecs(&wakeup, &expected, &wakeup_latency);
+
+    latency = timespec_to_long(&wakeup_latency);
+    hist.add(latency);
+    timeSeries.add(latency);
+  }
+
+  hist.saveHist(params.hist_filename);
+  hist.saveTopN(params.topn_filename);
+  timeSeries.save(params.timeseries_filename);
+
+  rclcpp::shutdown();
+  return 0;
+}
+
+Params get_params(int argc, char *argv[]) {
+  Params params;
+  int realtime_child = 0; // default: non-realtime
+
+  const struct option longopts[] = {
+      {"realtime_child_thread", no_argument, &realtime_child, 1},
+      {"non_realtime_child_thread", no_argument, &realtime_child, 0},
+      {"hist_filename", required_argument, 0, 'h'},
+      {"topn_filename", required_argument, 0, 'n'},
+      {"timeseries_filename", required_argument, 0, 't'},
+      {0, 0, 0, 0},
+  };
+
+  opterr = 0;
+  optind = 1;
+  int longindex = 0;
+  const std::string optstring = "+";
+  int c;
+
+  while ((c = getopt_long(argc, argv, optstring.c_str(), longopts,
+                          &longindex)) != -1) {
+    switch (c) {
+    case ('h'):
+      params.hist_filename = optarg;
+      break;
+    case ('n'):
+      params.topn_filename = optarg;
+      break;
+    case ('t'):
+      params.timeseries_filename = optarg;
+      break;
+    }
+  }
+  params.realtime_child = realtime_child;
+
+  argc -= optind - 2;
+  argv += optind - 2;
+
+  cout << argv[1] << endl;
+  if (rttest_read_args(argc, argv) != 0) {
+    perror("Couldn't read arguments for rttest");
+  }
+  rttest_get_params(&params.rt);
+  return params;
+}
